fe76585ef3a37c4869fd19c6ffbbe955819cc7c1
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2025-08-24 22:29:43 +0530

    css issue fix

--- handpicked-client/tailwind.config.mjs
+++ handpicked-client/tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,mdx}", // look inside src
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{astro,html,js,jsx,ts,tsx,mdx}",
+    "./pages/**/*.{astro,html,js,jsx,ts,tsx,mdx}",
+    "./layouts/**/*.{astro,html,js,jsx,ts,tsx,mdx}",
   ],
   theme: {
     extend: {},
