09f81dfe07cf4cea86f010858b46912919ee3053
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2025-08-26 16:49:40 +0530

    Client App Backend commit

--- handpicked_backend/index.js
+++ handpicked_backend/index.js
@@ -109,7 +109,7 @@ app.use((err, req, res, next) => {
   res.status(status).json({ error: message });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 // app.listen(PORT, () => {
 //   console.log(`✅ Server running on port ${PORT}`);
 // });
